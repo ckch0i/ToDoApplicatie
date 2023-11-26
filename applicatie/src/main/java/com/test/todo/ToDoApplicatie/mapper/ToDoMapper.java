43e866eca750a816e41323a68fda245efebd3832
CK <CK>
2023-11-26 23:34:03 +0100

    Losse mapper gebouwd voor mapping

---
+++ applicatie/src/main/java/com/test/todo/ToDoApplicatie/mapper/ToDoMapper.java
@@ -0,0 +1,28 @@
+package com.test.todo.ToDoApplicatie.mapper;
+
+import com.test.todo.ToDoApplicatie.entity.ToDo;
+import com.test.todo.ToDoApplicatie.model.ToDoDTO;
+import lombok.experimental.UtilityClass;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@UtilityClass
+public class ToDoMapper {
+
+    public static ToDo mapDTONaarEntity(ToDoDTO toDoDTO){
+        ToDo entity = new ToDo();
+        entity.setOmschrijving(toDoDTO.getOmschrijving());
+        entity.setAangemaaktOp(Instant.now());
+        entity.setUniekeId(UUID.randomUUID().toString());
+        return entity;
+    }
+
+    public static ToDoDTO mapEntityNaarDTO(ToDo todo){
+        ToDoDTO dto = new ToDoDTO();
+        dto.setOmschrijving(todo.getOmschrijving());
+        dto.setAangemaaktOp(todo.getAangemaaktOp().toString());
+        dto.setUniekeId(todo.getUniekeId());
+        return dto;
+    }
+}
--- applicatie/src/main/java/com/test/todo/ToDoApplicatie/service/ToDoService.java
+++ applicatie/src/main/java/com/test/todo/ToDoApplicatie/service/ToDoService.java
@@ -6,10 +6,11 @@ import com.test.todo.ToDoApplicatie.repository.ToDoRepository;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
+
+import static com.test.todo.ToDoApplicatie.mapper.ToDoMapper.mapDTONaarEntity;
+import static com.test.todo.ToDoApplicatie.mapper.ToDoMapper.mapEntityNaarDTO;
 
 @Service
 public class ToDoService {
@@ -17,7 +18,7 @@ public class ToDoService {
     @Resource
     ToDoRepository repository;
 
-    public ToDoDTO maakDTO(ToDoDTO toDoDTO){
+    public ToDoDTO maakDTO(ToDoDTO toDoDTO) {
         ToDo entity = mapDTONaarEntity(toDoDTO);
         ToDo repositoryResult = repository.save(entity);
         ToDoDTO result = new ToDoDTO();
@@ -27,32 +28,17 @@ public class ToDoService {
 
     public ToDoDTO zoekToDo(String uniekeId) {
         ToDo result = repository.findByUniekeId(uniekeId);
-        if (result != null){
+        if (result != null) {
             return mapEntityNaarDTO(result);
         }
         return null;
     }
+
     public List<ToDoDTO> haalAlleToDo() {
         Iterable<ToDo> gevondenToDo = repository.findAll();
         List<ToDoDTO> result = new ArrayList<>();
-        gevondenToDo.forEach( todo -> result.add(mapEntityNaarDTO(todo)));
+        gevondenToDo.forEach(todo -> result.add(mapEntityNaarDTO(todo)));
         return result;
-    }
-
-    private ToDo mapDTONaarEntity(ToDoDTO toDoDTO){
-        ToDo entity = new ToDo();
-        entity.setOmschrijving(toDoDTO.getOmschrijving());
-        entity.setAangemaaktOp(Instant.now());
-        entity.setUniekeId(UUID.randomUUID().toString());
-        return entity;
-    }
-
-    private ToDoDTO mapEntityNaarDTO(ToDo todo){
-        ToDoDTO dto = new ToDoDTO();
-        dto.setOmschrijving(todo.getOmschrijving());
-        dto.setAangemaaktOp(todo.getAangemaaktOp().toString());
-        dto.setUniekeId(todo.getUniekeId());
-        return dto;
     }
 
 }
