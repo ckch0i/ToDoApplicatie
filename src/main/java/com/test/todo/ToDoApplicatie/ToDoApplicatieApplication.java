0ab1628ecd20976cb4e2a98a63904f0bceafb38a
CK <CK>
2023-11-26 20:00:19 +0100

    spring-boot initializer gebruikt voor applicatie opzet

---
+++ src/main/java/com/test/todo/ToDoApplicatie/ToDoApplicatieApplication.java
@@ -0,0 +1,13 @@
+package com.test.todo.ToDoApplicatie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApplicatieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApplicatieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/todo/ToDoApplicatie/ToDoApplicatieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.todo.ToDoApplicatie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicatieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
