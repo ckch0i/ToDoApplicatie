ab04a0422805010561cdc96ab4aedb969d98705d
Jin Choi <h_y_choi@hotmail.com>
2023-11-28 23:16:56 +0100

    TodoStepDefs toegevoegd

--- integratietesten-cucumber/src/test/java/ToDoStepDefs.java
+++ integratietesten-cucumber/src/test/java/ToDoStepDefs.java
@@ -1,17 +1,22 @@
 import io.cucumber.java.en.Given;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class ToDoStepDefs {
-    private static final String BASE_URL = "http://localhost:8080/test";
+    private static final String BASE_URL = "http://localhost:8080/todo";
     @Given("ik een Todo met omschrijving {string} verstuur")
     public void ikEenTodoMetOmschrijvingVerstuur(String omschrijving) {
 
-        RestAssured.baseURI = "http://localhost:8080/test";
-        RequestSpecification request2 = RestAssured.given();
+        RestAssured.baseURI = "http://localhost:8080/todo";
+        RequestSpecification request2 = RestAssured.given().body("{\"omschrijving\" : \"vuilnis ophalen\"}").accept(ContentType.JSON)
+                .contentType(ContentType.JSON);
         Response response = request2.post();
         System.out.println("Response is: " + response.asPrettyString());
+        //TODO:Unieke ID vasthouden in eigen string
+        //Nieuwe Get request met deze unieke ID
+        //Verify omschrijving met verwachte result
     }
 
 }
